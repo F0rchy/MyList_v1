56be07323b97b1c0d3b4c3411965efab815f6de0
Artem <theartemka6@mail.ru>
2020-10-13 20:39:01 +0300

    Added subclass Item

---
+++ List.java
@@ -0,0 +1,42 @@
+package com.max.idea;
+
+public class List {
+    class Item {
+        private int value;
+        private Item next;
+
+        public Item(int value) {
+            this.value = value;
+            this.next = null;
+        }
+
+        public boolean hasNext() {
+            if (this.next != null) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Item getNext() {
+            return next;
+        }
+
+        public void setNext(Item next) {
+            this.next = next;
+        }
+
+        public String toString() {
+            return Long.toString(this.value);
+        }
+    }
+}
